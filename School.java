278c366daac5553063f6909bb90f7566fedb6df5
saravanan0404 <121876958+saravanan0404@users.noreply.github.com>
2023-01-30 20:37:40 +0530

    Add files via upload

---
+++ School.java
@@ -0,0 +1,18 @@
+class School
+{
+int Mark;
+static String school_name="Antony's primary school";
+void conduct_exam()
+{
+System.out.println("conduct_exam");
+}
+void publish_result(int mark)
+{
+System.out.println("pass"+mark);
+}
+private void salary(int amount)
+{
+System.out.println("salary:"+amount);
+}
+}
+
