5a6924942914c9896c2ced1f9dcb03d91e4066da
saravanan <saravananu4420@gmail.com>
2023-01-30 20:32:30 +0530

    second program

---
+++ Teacher.java
@@ -0,0 +1,12 @@
+class Teacher
+{
+public int salary;
+public static void main(String[] args)
+{
+School teacher=new School();
+teacher.conduct_exam();
+teacher.publish_result(75);
+teacher.salary(15000);
+System.out.println(School.school_name);
+}
+}
